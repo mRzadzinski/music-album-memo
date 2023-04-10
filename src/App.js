ffdbcc8b4f38e6d540aea4a46b6a56f28c025fdc
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-04-10 17:29:31 +0200

    Create and style header

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './styles/App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<div className='header'>
+				<div className='all-albums'>All &nbsp;Albums</div>
+				<div className='title'>Music &nbsp; Album &nbsp; Memo</div>
+        <div className='empty'></div>
+			</div>
+      <div className="gameplay">
+        <div className="score">Score 0/10</div>
+      </div>
+		</div>
+	);
+}
+
+export default App;
